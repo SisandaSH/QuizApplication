1de86fe614d5f70faaf21e661dbfb87a1faea2a4
thamesssa <thamesssa@outlook.com>
2025-01-21 06:06:21 +0200

    implemented the getUserInput() funtion

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Service implements QuestionImpl{
     ArrayList <Questions> questions = new ArrayList<>();
@@ -21,7 +22,22 @@ public class Service implements QuestionImpl{
 
     @Override
     public void getUserInput() {
-
+        Scanner scanner = new Scanner(System.in);
+        for (Questions question : questions) {
+            System.out.println(question.getQuestions());
+            String[] options = question.getOptions();
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ": " + options[i]);
+            }
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.nextLine();
+            if (userAnswer.equalsIgnoreCase(question.getAnswer())) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Incorrect!");
+            }
+        }
+        scanner.close();
     }
 
     @Override
